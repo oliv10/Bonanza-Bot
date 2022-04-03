024b65650a79c1fb72b984cd2a79ce3057f227b0
Oliver Scotten <revi99@me.com>
2022-04-03 18:14:01 -0500

    Ability to get Aircraft data consistently and without error.

--- aircraft.py
+++ aircraft.py
@@ -24,24 +24,22 @@ class AirData():
         self.position_source = position_source
         
     def __str__(self) -> str:
-        return self.icao24
-        pass
+        return str([self.icao24, self.callsign, self.origin_country, self.time_position, self.last_contact, self.longitude, self.latitude, self.baro_altitude, self.on_ground, self.velocity, self.heading, self.vertical_rate, self.sensors, self.geo_altitude, self.squawk, self.spi, self.position_source])
 
 class Aircraft(threading.Thread):
     
     def __init__(self, icao24: str, username: str = None, password: str = None) -> None:
-        threading.Thread.__init__(self)
+        threading.Thread.__init__(self, daemon=True)
         self.icao24 = icao24
         self.API = OpenSkyApi(username, password)
         self.currentAirData = None
         
     def run(self):
-        self.currentAirData = self.getAirData()
-        time.sleep(10)
+        while True:
+            self.currentAirData = self.getAirData()
+            time.sleep(10)
         
     def getAirData(self) -> AirData:
         states = self.API.get_states(icao24=self.icao24)
-        print(states)
         dictionary = states.states[0].__dict__
-        print(dictionary)
         return AirData(dictionary.get("icao24"), dictionary.get("callsign"), dictionary.get("origin_country"), dictionary.get("time_position"), dictionary.get("last_contact"), dictionary.get("longitude"), dictionary.get("latitude"), dictionary.get("baro_altitude"), dictionary.get("on_ground"), dictionary.get("velocity"), dictionary.get("heading"), dictionary.get("vertical_rate"), dictionary.get("sensors"), dictionary.get("geo_altitude"), dictionary.get("squawk"), dictionary.get("spi"), dictionary.get("position_source"))
