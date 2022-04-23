394d303a74944e2daec076b64d247967a33985da
Oliver Scotten <revi99@me.com>
2022-04-23 13:06:31 -0500

    Import Source Code

---
+++ v1.0/aircraft.py
@@ -0,0 +1,83 @@
+import time
+import threading
+from opensky_api import OpenSkyApi
+
+class AirData():
+    
+    def __init__(self, icao24: str = None, callsign: str = None, origin_country: str = None, time_position: str = None, last_contact: str = None, longitude: str = None, latitude: str = None, baro_altitude: str = None, on_ground: str = None, velocity: str = None, heading: str = None, vertical_rate: str = None, sensors: str = None, geo_altitude: str = None, squawk: str = None, spi: str = None, position_source: str = None) -> None:
+        self.icao24 = icao24
+        self.callsign = callsign
+        self.origin_country = origin_country
+        self.time_position = time_position
+        self.last_contact = last_contact
+        self.longitude = longitude
+        self.latitude = latitude
+        self.baro_altitude = baro_altitude
+        self.on_ground = on_ground
+        self.velocity = velocity
+        self.heading = heading
+        self.vertical_rate = vertical_rate
+        self.sensors = sensors
+        self.geo_altitude = geo_altitude
+        self.squawk = squawk
+        self.spi = spi
+        self.position_source = position_source
+        
+    def getLastContact(self):
+        try:
+            return int(self.last_contact)
+        except TypeError:
+            return None
+        
+    def getLongitude(self):
+        return self.longitude
+    
+    def getLatitude(self):
+        return self.latitude
+    
+    def getBaroAltitude(self):
+        return self.baro_altitude
+    
+    def getLanded(self):
+        return self.on_ground
+        
+    def getVelocity(self):
+        try:
+            return int(self.velocity * 1.944) # Adjust to Knots
+        except TypeError:
+            return None
+        
+    def getVerticalRate(self):
+        try:
+            return int(self.vertical_rate * 197) # Adjust to Ft/Min
+        except TypeError:
+            return None
+        
+    def __eq__(self, other) -> bool:
+        return self.last_contact == other.last_contact
+        
+    def __str__(self) -> str:
+        return str([self.icao24, self.callsign, self.origin_country, self.time_position, self.last_contact, self.longitude, self.latitude, self.baro_altitude, self.on_ground, self.velocity, self.heading, self.vertical_rate, self.sensors, self.geo_altitude, self.squawk, self.spi, self.position_source])
+
+class Aircraft(threading.Thread):
+    
+    def __init__(self, icao24: str, username: str = None, password: str = None) -> None:
+        threading.Thread.__init__(self, daemon=True)
+        self.icao24 = icao24
+        self.API = OpenSkyApi(username, password)
+        self.currentAirData = AirData()
+        self.previousAirData = AirData()
+        
+    def run(self):
+        while True:
+            self.previousAirData = self.currentAirData
+            self.currentAirData = self.getAirData()
+            time.sleep(10)
+        
+    def getAirData(self) -> AirData:
+        states = self.API.get_states(icao24=self.icao24)
+        try:
+            dictionary = states.states[0].__dict__
+        except IndexError:
+            return AirData()
+        return AirData(dictionary.get("icao24"), dictionary.get("callsign"), dictionary.get("origin_country"), dictionary.get("time_position"), dictionary.get("last_contact"), dictionary.get("longitude"), dictionary.get("latitude"), dictionary.get("baro_altitude"), dictionary.get("on_ground"), dictionary.get("velocity"), dictionary.get("heading"), dictionary.get("vertical_rate"), dictionary.get("sensors"), dictionary.get("geo_altitude"), dictionary.get("squawk"), dictionary.get("spi"), dictionary.get("position_source"))
---
+++ v1.0/geocode.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+class LocationData:
+    
+    def __init__(self, place_id = None, licence = None, osm_type = None, osm_id = None, lat = None, lon = None, display_name = None, address = None, boundingbox = None) -> None:
+        self.place_id = place_id
+        self.licence = licence
+        self.osm_type = osm_type
+        self.osm_id = osm_id
+        self.lat = lat
+        self.lon = lon
+        self.display_name = display_name
+        self.address = address
+        self.boundingbox = boundingbox
+        
+    def getAddress(self):
+        return list(self.address.values())
+
+    def __str__(self) -> str:
+        return str([self.place_id, self.licence, self.osm_type, self.osm_id, self.lat, self.lon, self.display_name, self.address, self.boundingbox])
+
+class Geocoode:
+    
+    def __init__(self, api) -> None:
+        self.API = api
+        
+    def getReverse(self, lon, lat) -> LocationData:
+        url = f"https://us1.locationiq.com/v1/reverse.php?key={str(self.API)}&lat={str(lat)}&lon={str(lon)}&zoom=12&normalizeaddress=1&format=json"
+        response = requests.get(url)
+        data = json.loads(response.content.decode(response.encoding))
+        return self.getLocationData(data)
+    
+    def getLocationData(self, location: dict) -> LocationData:
+        return LocationData(place_id = location.get("place_id"), licence = location.get("licence"), osm_type = location.get("osm_type"), osm_id = location.get("osm_id"), lat = location.get("lat"), lon = location.get("lon"), display_name = location.get("display_name"), address = location.get("address"), boundingbox = location.get("boundingbox"))
+
